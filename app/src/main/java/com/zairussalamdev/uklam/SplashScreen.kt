a841685b7a1327a7c0d0135c39e68ad743f5416f
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-07-17 14:02:46 +0700

    add firebase SDK

--- app/src/main/java/com/zairussalamdev/uklam/SplashScreen.kt
+++ app/src/main/java/com/zairussalamdev/uklam/SplashScreen.kt
@@ -1,12 +1,13 @@
 package com.zairussalamdev.uklam
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.*
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+        startActivity<MainActivity>()
     }
 }
