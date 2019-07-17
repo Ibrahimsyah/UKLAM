f47823252fd0790075993aef9a836cfe78313ab6
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-07-17 10:55:03 +0700

    add Splash Screen

--- app/src/main/java/com/zairussalamdev/uklam/SplashScreen.kt
+++ app/src/main/java/com/zairussalamdev/uklam/SplashScreen.kt
@@ -3,10 +3,10 @@ package com.zairussalamdev.uklam
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
